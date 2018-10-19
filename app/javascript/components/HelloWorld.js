4e71bad1fe1ec447d30df14b9040f502b8093dd9
intelsy <intel.sy.408@gmail.com>
2018-10-20 01:48:22 +0900

    Initial Commit

---
+++ app/javascript/components/HelloWorld.js
@@ -0,0 +1,19 @@
+import React from "react"
+import PropTypes from "prop-types"
+class HelloWorld extends React.Component {
+  render () {
+    return (
+        // Greeting: {this.props.greeting}
+        <div>
+        <h1>instantgram</h1>
+        <p>Otintitns</p>
+        aoaoaoa
+        </div>
+    );
+  }
+}
+
+// HelloWorld.propTypes = {
+//   greeting: PropTypes.string
+// };
+export default HelloWorld
